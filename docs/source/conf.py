0c2843f6b5995ef0b3097d746d4527fe922c7fc3
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2020-02-21 23:19:21 +0530

    sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,8 +18,8 @@
 # -- Project information -----------------------------------------------------
 
 project = 'overload'
-copyright = '2020, Arpit Bhayani, Simon Brunning'
-author = 'Arpit Bhayani, Simon Brunning'
+copyright = '2020, Arpit Bhayani, Simon Burnninng'
+author = 'Arpit Bhayani, Simon Burnninng'
 
 
 # -- General configuration ---------------------------------------------------
@@ -36,7 +36,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
